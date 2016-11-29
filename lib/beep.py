d4cacb60d07caa6662698eb192cd24aa9784503b
Michael Davies <michael@the-davies.net>
2016-11-29 18:28:43 +1030

    Adding sound + general cleanup

---
+++ lib/beep.py
@@ -0,0 +1,91 @@
+
+from machine import Pin, PWM
+import time
+tempo = 5
+
+tones = {
+    'B0': 31,
+    'C1': 33,
+    'CS1': 35,
+    'D1': 37,
+    'DS1': 39,
+    'E1': 41,
+    'F1': 44,
+    'FS1': 46,
+    'G1': 49,
+    'GS1': 52,
+    'A1': 55,
+    'AS1': 58,
+    'B1': 62,
+    'C2': 65,
+    'CS2': 69,
+    'D2': 73,
+    'DS2': 78,
+    'E2': 82,
+    'F2': 87,
+    'FS2': 93,
+    'G2': 98,
+    'GS2': 104,
+    'A2': 110,
+    'AS2': 117,
+    'B2': 123,
+    'C3': 131,
+    'CS3': 139,
+    'D3': 147,
+    'DS3': 156,
+    'E3': 165,
+    'F3': 175,
+    'FS3': 185,
+    'G3': 196,
+    'GS3': 208,
+    'A3': 220,
+    'AS3': 233,
+    'B3': 247,
+    'C4': 262,
+    'CS4': 277,
+    'D4': 294,
+    'DS4': 311,
+    'E4': 330,
+    'F4': 349,
+    'FS4': 370,
+    'G4': 392,
+    'GS4': 415,
+    'A4': 440,
+    'AS4': 466,
+    'B4': 494,
+    'C5': 523,
+    'CS5': 554,
+    'D5': 587,
+    'DS5': 622,
+    'E5': 659,
+    'F5': 698,
+    'FS5': 740,
+    'G5': 784,
+    'GS5': 831,
+    'A5': 880,
+    'AS5': 932,
+    'B5': 988,
+}
+
+
+def play_notes(notes):
+    n = notes.split(',')
+    r = [8] * len(n)
+    _play_song(n, r)
+
+
+def play_mockingjay():
+    # Mockingjay
+    notes = ['G5', 'AS5', 'A5', 'D5']
+    rhythm = [8, 8, 8, 8]
+    _play_song(notes, rhythm)
+
+
+def _play_song(notes, rhythm):
+    try:
+        beeper = PWM(Pin(14, Pin.OUT), freq=440, duty=512)
+        for tone, length in zip(notes, rhythm):
+            beeper.freq(tones[tone.upper()])
+            time.sleep(tempo/length)
+    finally:
+        beeper.deinit()
--- projects/Frankenbot/frankenbot.py
+++ projects/Frankenbot/frankenbot.py
@@ -7,7 +7,10 @@
 # curl -i -X PUT http://192.168.0.128:8080/turn/right
 # curl -i -X PUT http://192.168.0.128:8080/move/10
 
+import beep
 import drive
+import machine
+import micropython
 import ws
 
 
@@ -30,8 +33,22 @@ class Frankenbot:
         else:
             return "Unknown direction"
 
+    def status(self, data):
+        s = "I'm " + str(machine.unique_id()) + "\n"
+        return s
+
+    def do_beep(self, data):
+        beep.play_notes(data)
+        return "I like music!"
+
+
 w = ws.WebServer()
 f = Frankenbot()
 w.register_url("PUT", "turn", f.turn)
 w.register_url("PUT", "move", f.move)
+w.register_url("PUT", "beep", f.do_beep)
+w.register_url("GET", "status", f.status)
+
+beep.play_mockingjay()
+
 w.start()
